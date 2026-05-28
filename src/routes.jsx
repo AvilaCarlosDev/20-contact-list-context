c82f46f8acc0fc381a26256bb64d23fa94bd6bd2
Carlos Avila <avilavaleriocarlos@gmail.com>
2026-05-27 23:53:05 -0400

    Refactor routes and layout structure in routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,30 +1,35 @@
-// Import necessary components and functions from react-router-dom.
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
+import { BackendURL } from "./component/backendURL";
 
-import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-} from "react-router-dom";
-import { Layout } from "./pages/Layout";
-import { Home } from "./pages/Home";
-import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
+import { Contacts } from "./pages/Contacts";
+import { AddContact } from "./pages/AddContact";
+import injectContext from "./store/appContext";
 
-export const router = createBrowserRouter(
-    createRoutesFromElements(
-    // CreateRoutesFromElements function allows you to build route elements declaratively.
-    // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
-    // Root, on the contrary, create a sister Route, if you have doubts, try it!
-    // Note: keep in mind that errorElement will be the default page when you don't get a route, customize that page to make your project more attractive.
-    // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
+import { Navbar } from "./component/navbar";
 
-      // Root Route: All navigation will start from here.
-      <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
+//create your first component
+const Layout = () => {
+    	//the basename is used when your project is published in a subdirectory and not in the root of the domain
+    	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+    	const basename = process.env.BASENAME || "";
 
-        {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-        <Route path= "/" element={<Home />} />
-        <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
-        <Route path="/demo" element={<Demo />} />
-      </Route>
-    )
-);
+    	return (
+            		<div>
+                    			<BrowserRouter basename={basename}>
+                                				<ScrollToTop>
+                                                					<Navbar />
+                                                					<Routes>
+                                                                    						<Route element={<Contacts />} path="/" />
+                                                                    						<Route element={<AddContact />} path="/add" />
+                                                                    						<Route element={<AddContact />} path="/edit/:id" />
+                                                                    						<Route element={<h1>Not found!</h1>h1>} />
+                                                                                            </Route>Routes>
+                                                                    </Routes>ScrollToTop>
+                                                </ScrollToTop>BrowserRouter>
+                                </BrowserRouter>div>
+                    	);
+                        };
+                    
+                    export default injectContext(Layout);</div>
