70c9cd806024388d6117808c18d6bd827a229c27
Carlos Avila <avilavaleriocarlos@gmail.com>
2026-05-28 00:08:13 -0400

    Add appContext for state management with React

---
+++ src/store/appContext.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import getState from "../store.js";
+
+export const Context = React.createContext(null);
+
+const injectContext = (PassedComponent) => {
+    const StoreWrapper = (props) => {
+          const [state, setState] = useState(
+                  getState({
+                            getStore: () => state.store,
+                            getActions: () => state.actions,
+                            setStore: (updatedStore) =>
+                                        setState((prevState) => ({
+                                                      store: Object.assign(prevState.store, updatedStore),
+                                                      actions: { ...prevState.actions },
+                                        })),
+                  })
+                );
+
+          return (
+                  <Context.Provider value={state}>
+                            <PassedComponent {...props} />
+                  </Context.Provider>Context.Provider>
+                );
+    };
+    return StoreWrapper;
+};
+
+export default injectContext;
